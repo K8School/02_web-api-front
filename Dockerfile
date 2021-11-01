3e88ae4af32116f5ebab2d71163daefc04ee07a6
Reuben Cleetus <rcleetus@gmail.com>
2021-11-01 19:27:35 -0400

    Updated Dockerfile, app, requests

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,13 @@
-FROM ubuntu:latest
-RUN apt-get update -y
-RUN apt-get install -y python3.9
-RUN apt-get install -y pip python-dev build-essential
+FROM python:3-slim-buster
+
+EXPOSE 8080/TCP
+
+# Keep Python from generating .pyc files in the container
+ENV PYTHONDONTWRITEBYTECODE=0
+
+# Turns off buffering for easier container logging
+ENV PYTHONUNBUFFERED=1
+
 COPY . /app
 WORKDIR /app
 RUN pip install -r requirements.txt
--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 from flask import Flask
 from flask import render_template
+import requests
+from dotenv import load_dotenv
+import os
+import json
+
+load_dotenv()  # take environment variables from .env.
+
+# Code of your application, which uses environment variables (e.g. from `os.environ` or
+# `os.getenv`) as if they came from the actual environment.
+
 
 app = Flask(__name__)
 
@@ -12,6 +22,14 @@ def hello():
 def hello_you(name):
     #return 'Hello, World! This is v4!'
     return render_template('index.html', msg=f"Hello {name}!", title="Hello from a Docker Container!")
+
+@app.route('/microservices')
+def microservice():
+    quote_url = os.environ.get("API_QUOTES")
+    r = requests.get(quote_url + "/quote_sentiment").text  
+    quote = json.loads(r)["phrase"]
+    sentiment = json.loads(r)["sentiment"]
+    return render_template('frame.html', quote=quote, sentiment=sentiment, time="now")
 
 
 @app.route('/universe')
@@ -31,4 +49,4 @@ def txtadd(num1, num2):
     return f"{sum}"
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80)
+    app.run(host="0.0.0.0", port=8080)
